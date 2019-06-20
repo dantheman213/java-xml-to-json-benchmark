d2ab684b14f7427be2a4d8ec6e7ad366df5f308e
Daniel Gillespie <daniel.gillespie@pluto.tv>
2019-06-20 13:43:44 -0700

    optimizations

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,11 +1,7 @@
 package app;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.json.XML;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
@@ -18,7 +14,7 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Reading test XML payload from file...");
-        var xmlRawPayload = readFile("/opt/payload.xml");
+        var xmlRawPayload = new String(Files.readAllBytes(Paths.get("/opt/payload.xml")));
 
         System.out.println(String.format("Started at %s.", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date())));
         long startTime = System.currentTimeMillis();
@@ -30,7 +26,7 @@ public class Application {
             var thread = new Thread(new Runnable() {
                 public void run() {
                     for (int j = 0; j < iterationCount; j++) {
-                        convertXmlToJson(xmlRawPayload);
+                        var json = XML.toJSONObject(xmlRawPayload);
                     }
                 }
             });
@@ -47,19 +43,5 @@ public class Application {
         long endTime = System.currentTimeMillis();
         System.out.println(String.format("Completed in %d seconds.", ((endTime - startTime) / 1000)));
         System.out.println(String.format("Finished at %s.", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date())));
-    }
-
-    private static void convertXmlToJson(String xml) {
-        try {
-            var json = XML.toJSONObject(xml);
-        } catch (JSONException e) {
-            System.out.println(e.toString());
-        }
-    }
-
-    private static String readFile(String path) throws IOException
-    {
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
-        return new String(encoded, Charset.defaultCharset());
     }
 }
